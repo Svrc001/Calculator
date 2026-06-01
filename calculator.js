3b9d6659603fd5202a4c582cb06c261ce21ceb0d
Srividya R. Chitrao <srchitrao@gmail.com>
2026-06-01 22:43:55 +0530

    Add math operation logic

--- calculator.js
+++ calculator.js
@@ -1,3 +1,16 @@
+let number1 = 0;
+let operator = '';
+let number2 = 0;
+let result = 0;
+
+let initial = true;
+let number2Exist = false;
+let number1Exist = false;
+let resultExist = false;
+
+let beforeDecimal = true;
+
+
 function add(n1, n2) {
     return n1 + n2;
 }
@@ -37,8 +50,133 @@ function operate(n1, n2, operator) {
             console.log("Invalid Operator");
     }
 
-    console.log(result);
+    const round = (num) => +num.toFixed(5);
+
+    return round(result);
 }
+
+function inputOperator() {
+    const operators = document.querySelectorAll('.operator-button');
+
+    operators.forEach(op => {
+        op.addEventListener('click', (event) => {
+            if(number2Exist && operator !== '') {
+                result = operate(number1, number2, operator);
+                number1 = 0;
+                number2 = 0;
+                number2Exist = false;
+                number1Exist = false;
+                resultExist = true;
+                updateOnScreen(result);
+            } 
+            if(resultExist && !number1Exist) {
+                number1 = result;
+                resultExist = false;
+                number1Exist = true;
+            }
+            
+            operator = event.target.innerHTML; 
+            updateOnScreen(operator);       
+        
+        });
+    });
+}
+
+function input() {
+    const digitInput = document.querySelectorAll('.digit-button');
+    
+    digitInput.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const digit = event.target.innerHTML;
+            
+            if(resultExist) {
+                operator = '';
+                updateOnScreen('');
+            }
+            if(operator === '') {
+                if(number1 === 0) {
+                    number1 = parseInt(digit);
+                } 
+                else {
+                    number1 = number1*10 + parseInt(digit);
+                }
+                number1Exist = true;
+                resultExist = false;
+            } else {
+                if(number2 === 0) {
+                    number2 = parseInt(digit);
+                }
+                else {
+                    number2 = number2*10 + parseInt(digit);
+                }
+                number2Exist = true;
+            }
+            updateOnScreen(digit);
+            initial = false;
+        });
+    });
+}
+
+function equalTo() {
+    const equals = document.querySelector('#equals-button');
+
+    equals.addEventListener('click', () => {
+        if(number2Exist) {
+            if(operator === '/' && number2 === 0) {
+                alert("Cannot divide by 0!");
+            }
+            else {
+                result = operate(number1, number2, operator);
+                number1 = 0;
+                number2 = 0;
+                number2Exist = false;
+                number1Exist = false;
+                resultExist = true;
+                updateOnScreen(result);
+            }
+        } else {
+            alert("Invalid format used.");
+        }
+    });
+}
+
+function updateOnScreen(value) {
+    const display = document.querySelector('.show');
+
+    if(initial || resultExist || value === ''){
+        display.textContent = value;
+    }
+    else if(display.textContent.at(-1) < '0' && value < '0') {
+        display.textContent = display.textContent.slice(0, -1) + value;
+    }
+    else {
+        display.textContent += value;
+    }
+}
+
+function clear() {
+    const clearButton = document.querySelector('#clear-button');
+
+    clearButton.addEventListener('click', () => {
+        number1 = 0;
+        number2 = 0;
+        result = 0;
+        operator = '';
+        
+        number1Exist = false;
+        number2Exist = false;
+        resultExist = false;
+
+        initial = true;
+        updateOnScreen('0');
+    });
+}
+
+input();
+inputOperator();
+equalTo();
+clear();
+
 
 
 
